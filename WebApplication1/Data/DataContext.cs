9e00d50ca1724f957b3dca2f3fccec0dcebb9553
VU <VU@DESKTOP-Q97A3A1>
2023-11-30 17:39:41 +0500

    Add project files.

---
+++ WebApplication1/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Entity;
+
+namespace WebApplication1.Data
+{
+    public class DataContext : IdentityDbContext<IdentityUser>
+    {
+        public DataContext(DbContextOptions<DataContext>options) : base(options)
+        {
+            
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Role> Roles { get; set; } 
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // builder.Seed();
+        }
+    }
+}
